ccc34c1a61df915cf8de776e3023e3c720d8920e
Mike Murray <m@murz.net>
2016-03-21 06:58:37 -0700

    use our fork of grpc

--- plugin/grpc/grpc.go
+++ plugin/grpc/grpc.go
@@ -48,7 +48,7 @@ import (
 // relative to the import_prefix of the generator.Generator.
 const (
 	contextPkgPath = "golang.org/x/net/context"
-	grpcPkgPath    = "google.golang.org/grpc"
+	grpcPkgPath    = "github.com/nourish/grpc"
 )
 
 func init() {
